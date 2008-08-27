df83b59513a27246eb40c4d57d447df9b4daab7c
aditya.joshi.2000 <aditya.joshi.2000@7141896b-bb51-0410-a161-b7c81ae5c018>
2008-08-27 17:06:17 +0000

    Improved logging (messages and use)

--- trunk/src/netscout.py
+++ trunk/src/netscout.py
@@ -7,31 +7,34 @@ Author: Aditya Joshi
 import sys
 import os
 import shutil
-from ConfigParser import ConfigParser
 import logging
-import sqlite3
 
-from utils import *
+import p2p
+import utils
 
- 
 def createDirStruct():
     """
     Create the directory struccture for holding network dumps. 
     A directory of the same name (if any) is deleted and recreated.
     """
     try:
-        dirstat=os.stat(getCfg('dataDir'))
-        shutil.rmtree(getCfg('dataDir'))
+        dirstat=os.stat(utils.getCfg('dataDir'))
+        shutil.rmtree(utils.getCfg('dataDir'))
     except:
         logging.info('Creating log directories')
-    os.mkdir(getCfg('dataDir'))
-    os.mkdir(getCfg('p2pDir'))
-    os.mkdir(getCfg('httpDir')) 
+    os.mkdir(utils.getCfg('dataDir'))
+    os.mkdir(utils.getCfg('p2pDir'))
+    os.mkdir(utils.getCfg('httpDir')) 
     
 def helpInfo():
     logging.info('nescout version 1.0')
     logging.info('Usage: netscout [-i|-r] <interface> [-reset] ')
     logging.info('Ensure you are root!')
+
+def verInfo():
+    logging.info('nescout version 1.0')
+    logging.critical('Ensure you are root!')
+
 
 def passiveAnalysis(conn):
     p2pmodel=p2p.P2PModel(netscout.interface,netscout.P2P_FOLDER)
@@ -45,14 +48,11 @@ def processArgs():
     paramList=sys.argv
     if ("-v" in paramList):
         """
-        In addtion the file logging, INFo and above messages will be logged to stdout
+        In addition the file logging, DEBUG and above messages will be logged to stdout
+        Edit the Stream logger (second in the handler array to update the logging level)
         """
-        console = logging.StreamHandler()
-        console.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(levelname)-8s: %(message)s')
-        console.setFormatter(formatter)
-        logging.getLogger('').addHandler(console)
-        logging.info('Screen logging on')
+        logging.getLogger().handlers[1].setLevel(logging.DEBUG)
+        logging.info('Detailed Screen logging on')
         
     if ("-h" in paramList):
         helpInfo()
@@ -66,7 +66,7 @@ def processArgs():
         Ensure that the mandatory parameter '-i' or '-r' is specfied
     """ 
     if (not("-i" in paramList or "-r" in paramList)):
-        logging.error("\n---ERROR: Either INTERFACE '-i' or REPORT '-r' is MANDATORY---")
+        logging.error("Either of INTERFACE '-i' or REPORT '-r' is MANDATORY")
         sys.exit()
     
     if("-i" in paramList):
@@ -75,16 +75,16 @@ def processArgs():
         try:
             interface=paramList[interfaceInd]
         except:
-            logging.error("\n---ERROR: NO INTERFACE VALUE SPECIFIED---")
+            logging.error("NO INTERFACE VALUE SPECIFIED")
             sys.exit()
-        logging.info("Netscout: Passive mode")
+        logging.info("netscout passive mode")
         utils.setConfigParam("interface", interface)
     else:  
-        logging.info("Netscout: Report mode")
+        logging.info("netscout report mode")
         report()
   
 def report():
-    conn = sqlite3.connect(utils.getCfg('nsDB'))
+    conn = utils.connectDB()
     conn.close()
     
 
@@ -92,8 +92,8 @@ def initDB():
     """
         Initialize database
     """
-    conn = connectDB()
-    schemaFile = open(getCfg('nsSchema'));
+    conn = utils.connectDB()
+    schemaFile = open(utils.getCfg('nsSchema'));
     conn.executescript(schemaFile.read())
     conn.commit()
     logging.info('Database reset successfully')
@@ -104,16 +104,26 @@ def netscout():
     """
     Main thread of execution
     """
-    logging.basicConfig(filename='netscout.log',format="%(levelname)-8s %(asctime)s \
-    Line %(lineno)-5d: %(message)s",level=logging.DEBUG)
+    
+    """
+    Configuring the logging framework
+    """
+    
+    logging.basicConfig(filename='netscout.log',datefmt="%d-%m-%Y %H:%M",\
+                        format="%(levelname)-8s %(asctime)+17s Line %(lineno)-4d %(module)s.%(funcName)-15s : %(message)s"\
+                        ,level=logging.DEBUG,filemode="a")
     logging.info('==========Initiating netscout logging==========')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(levelname)-8s: %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)
+    logging.debug('Screen logging on')
+    verInfo()
+    utils.readConfig()
     processArgs()
-    readConfig()
     createDirStruct()
-    #p2p.p2pModel(conn)
-    #p2pmodel=p2p.P2PModel(netscout.interface,netscout.P2P_FOLDER)
-    #p2pmodel.getHubInfo()
-    #p2pmodel.printP2PAnalysis()
+    
 
 """
 Invoke the main thread.
