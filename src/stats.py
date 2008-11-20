1ab3372e71e960271b5f2aca9c121789087b0bcf
aditya.joshi.2000 <aditya.joshi.2000@7141896b-bb51-0410-a161-b7c81ae5c018>
2008-11-20 06:51:30 +0000

    Added tech paper draft, topology dia, stat mode

--- src/stats.py
+++ src/stats.py
@@ -1,7 +1,48 @@
 """
 Statistical analysis
 """
+import logging
+
+import utils
+import const
+import host
 
 def statActivity():
-    pass
+    logging.debug("starting statistical mode")
+    #look for number of connections into
+    #look for bandwidth usage
+    #look for number of requests to specific hosts
+    os.system("tcpdump -s0 -i "+getCfg("interface")\
+		+" 'dst not "+getCfg("ignoreHost")\+"'"\
+		+" -c "+getCfg("connTo")+" -w - | "\
+		+" tshark -i - -T fields -e ip.dst |"\
+		+" sort > "+getCfg("dstCountFile"))
+    dstList = utils.fileToArray(getCfg("dstCountFile"))
+    uniqueDst = list(set(dstList))
+    highDstConn = []
+    for ip in uniqueDst:
+	if dstList.count(ip) >= int(getCfg("connToMin")):
+	    highDstConn.append(ip)
+    host.addHostActivity(ip,extractUserID(ip)) 
+
+
+def protectResource():
+    logging.debug("starting protection mode")
+    #look for number of connections into
+    #look for bandwidth usage
+    #look for number of requests to specific hosts
+    os.system("tcpdump -s0 -i "+getCfg("interface")\
+		+" 'dst  "+getCfg("impRes")\+"'"\
+		+" -c "+getCfg("prConnTo")+" -w - | "\
+		+" tshark -i - -T fields -e ip.src |"\
+		+" sort > "+getCfg("srcCountFile"))
+    srcList = utils.fileToArray(getCfg("srcCountFile"))
+    uniqueSrc = list(set(srcList))
+    highSrcConn = []
+    for ip in uniqueSrc:
+	if srcList.count(ip) >= int(getCfg("prConnToMin")):
+	    highSrcConn.append(ip)
+    host.addHostActivity(ip,extractUserID(ip)) 
+    
+
 
